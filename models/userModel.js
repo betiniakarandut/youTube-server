3eda0f5dd4d1afefe68d4a092423322baaf4bdbd
betiniakarandut <betiniakarandut@gmail.com>
2024-03-20 05:17:51 +0100

    feat: user models

---
+++ models/userModel.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+
+    },
+    email: {
+        required: true,
+        type: String,
+        lowercase: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+    },
+    roles: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user',
+    },
+    permissions: {
+        type: Array,
+        default: [],
+    },
+    phone: {
+        type: String,
+        validator: {
+            validator: function (value){
+                return /^\d{3}-\d{3}-\d{4}$/.test(value);
+            }
+        }
+    },
+    address: {
+        street: String,
+        city: String,
+        country: String,
+        postalcode: String,
+    }
+})
+
+export default userSchema;
