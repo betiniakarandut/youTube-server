799aab8bb62f9bba933bc9caafa551cf54163adf
betiniakarandut <betiniakarandut@gmail.com>
2024-03-21 03:33:09 +0100

    chore: Removed sensitive info

--- utils/configuration.js
+++ utils/configuration.js
@@ -10,6 +10,3 @@ export const PORT = process.env.PORT;
 export const secret_key = process.env.SECRET_KEY;
 
 export const dbConnStr = `mongodb+srv://${dbusername}:${pwd}@cluster0.z3afn56.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-
-// betiniakarandut
-// EzcuvLTT4gbA7JZF
