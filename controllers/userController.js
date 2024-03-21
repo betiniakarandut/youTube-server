a82eb67f994154265bc61ff34085aaa4d72f64ca
betiniakarandut <betiniakarandut@gmail.com>
2024-03-21 10:36:17 +0100

    chore: solved Error [ERR_HTTP_HEADERS_SENT]

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,69 +1,63 @@
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken';
-import userSchema from "../models/userModel.js";
+import User from "../models/userModel.js";
 import { secret_key } from '../utils/configuration.js';
 
 
 export const signUp = async (req, res) => {
     try {
-        const { username, email, password } = req;
-        const userExist = await userSchema.username.findOne({email})
+        const { username, email, password } = req.body;
+        const userExist = await User.findOne({email})
         if (userExist){
-            res.status(400).json({message:"User already exist exist"});
+            return res.status(400).json({message:"User already exist exist"});
         }
 
         const hashed_password = await bcrypt.hash(password, 10);
 
-        const newUser = {
+        const newUser = new User({
             email,
             hashed_password,
-            username
-        }
+            // username
+        })
 
         const payload = {
             userId: newUser._id,
             email: newUser.email
         }
-        const jwtToken = jwt.sign(payload, secret_key, {expires: '24h'})
+        const jwtToken = jwt.sign(payload, secret_key, {expires: '24h'});
 
 
-        await newUser.save().then(() => {
-            
-            res.status(200).json({jwtToken, userExist});
-        }).catch(() => {
-            res.status(400).json({message:"Failed to create YouTube user. Try again!"})
-        })  
+        await newUser.save();
+        return res.status(200).json({ jwtToken, userExist });
     } catch{
         (err) => {
-            res.status(500).json({message:`Server error: ${err}`})
+            return res.status(500).json({message:`Server error: ${err}`});
         }
     }
 }
 
 export const signIn = async (req, res) => {
     try {
-        const { username, email, password } = req;
-        const userExist = await userSchema.username.findOne({email})
+        const { username, email, password } = req.body;
+        const userExist = await User.findOne({email})
         if (!userExist){
-            res.status(401).json({message:"Invalid credentials. Check and try again"});
+            return res.status(401).json({message:"Invalid credentials. Check and try again"});
         }
 
         const passwordMatch = await bcrypt.compare(password, userExist.hashed_password);
         if (!passwordMatch) {
-            res.status(401).json({message: "Invalid password. Check and try again"});
+            return res.status(401).json({message: "Invalid password. Check and try again"});
         }
 
 
         const payload = {
-            userId: newUser._id,
-            email: newUser.email
+            userId: userExist._id,
+            email: userExist.email
         }
         const jwtToken = jwt.sign(payload, secret_key, {expires: '24h'})
 
-        res.status(200).json({jwtToken, userExist});
-    } catch{
-        (err) => {
-            res.status(500).json({message:`Server error: ${err}`})
-        }
+        return res.status(200).json({jwtToken, userExist});
+    } catch(err){
+            return res.status(500).json({message:`Server error: ${err}`})
     }
 }
