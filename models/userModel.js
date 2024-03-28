0612bea5b633d4fa5c81109ba599a4d70605512b
betiniakarandut <betiniakarandut@gmail.com>
2024-03-28 03:59:13 +0100

    chore:remove required field

--- models/userModel.js
+++ models/userModel.js
@@ -3,13 +3,11 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: true,
         unique: true,
         trim: true,
     },
     email: {
         type: String,
-        required: true,
         unique: true,
         trim: true,
         lowercase: true,
