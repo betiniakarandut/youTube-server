8afd202059a0290a632e33553a4050640cb85156
betiniakarandut <betiniakarandut@gmail.com>
2024-03-30 00:01:40 +0100

    Feat: middlewarefor video uploads

---
+++ middlewares/userAuth.js
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+
+dotenv.config();
+
+export const middlewareAuth = async (req, res, next) => {
+    try {
+        // get the jwt token from authorization headers
+        const token = req.headers.authorization.split('')[1];
+
+        // check if decoded token is valid against a secret key
+        decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+    
+        if (!decodedToken) {
+            return res.status(401).json({
+                status: "FAILED",
+                message: "User unauthorized"
+            });
+        }
+        // assign user to the id associated with the token
+        const user = await User.findById(decodedToken.userId);
+        req.user = user;
+
+        next();
+
+    } catch (error) {
+        console.log(error)
+        return res.status(401).json({
+            status: "FAILED",
+            message: `Authentication failed ${error}`
+        });
+    }
+}
