262d2b40ab26a2afbb55c98182802933f63d9342
betiniakarandut <betiniakarandut@gmail.com>
2024-03-30 04:19:21 +0100

    feat: checks

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,42 +1,52 @@
-import User from "../models/userModel.js";
 import Video from "../models/videoModel.js";
 
 
 export const videoUpload = async (req, res) => {
-    const { title, description } = req.body;
-    // check for authenticated user
-    if (!User.verified) {
-        return res.status(403).json({
-            status: "FAILED",
-            message: "User is not uthorized"
-        })
-    } else if (! _id) {
-        return res.status(403).json({
-            status: "FAILED",
-            message: "Invalid user id"
-        })
-    }
+    try {
+        const { title, description } = req.body;
 
-    const newVideo = new Video({
+        if (title == '' || description == '') {
+            return res.status(400).json({
+                status: "FAILED",
+                message: "Invalid credentials or fields cannot be empty"
+            });
+        }
+
+        if (title.length < 5 || title.length > 100 || description.length < 10 || description.length > 500) {
+            return res.status(400).json({
+                status: "FAILED",
+                message: "Title must be between 5 and 100 characters. Description must be between 10 and 500 characters",
+            })
+        }
         
-        title: title,
-        description: description,
-        creatorId: req.User._id,
-        createdAt: Date.now(),
-    })
-     
-    const savedVideo = await newVideo.save()
-    .then((success) => {
-        console.log(success);
-        return res.status(200).json({
-            status: "Success",
-            message: "Video was uploaded successfully",
+        if (!req.user) {
+            return res.status(401).json({
+                status: "FAILED",
+                message: "User is not authenticated"
+            })
+        }
+
+        const newVideo = new Video ({
+            title: title,
+            description: description,
+            creatorId: req.user._id,
+            createdAt: Date.now(),
         })
-    }).catch((err) => {
+
+         const savedVideo = await newVideo.save()
+         console.log (newVideo);
+
+         res.status(201).json({
+            status: "Created!",
+            message: "Video was uploaded successfully",
+            video: savedVideo.filePath,
+         })
+        
+    } catch (error) {
         console.log(err);
         return res.status(401).json({
             status: "FAILED",
             message: "Failed to upload video"
-        })
-    })
+        });
+    }
 }
