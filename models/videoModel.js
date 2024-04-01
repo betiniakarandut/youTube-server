4b53029d95d741a1e7ee294b3f900d4fe455f7c2
betiniakarandut <betiniakarandut@gmail.com>
2024-04-01 09:51:19 +0100

    chore: added videoId

--- models/videoModel.js
+++ models/videoModel.js
@@ -5,10 +5,8 @@ const videoModelSchema = new mongoose.Schema({
     description: String,
     filePath: String,
     creatorId: String,
+    videoId: String,
     createdAt: Date,
-    views: Number,
-    comments: String,
-    likes: Number,
 });
 
 const Video = mongoose.model("Video", videoModelSchema);
