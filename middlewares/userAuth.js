1a9fa6c937bb09350429b36f3e0d0d2423035acf
betiniakarandut <betiniakarandut@gmail.com>
2024-04-02 08:40:07 +0100

    feat: token regeneration

--- middlewares/userAuth.js
+++ middlewares/userAuth.js
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-import jwt from "jsonwebtoken";
+import jwt, { decode } from "jsonwebtoken";
 import User from "../models/userModel.js";
 
 dotenv.config();
@@ -8,6 +8,7 @@ export const middlewareAuth = async (req, res, next) => {
     try {
         // get the jwt token from authorization headers
         const token = req.headers.authorization.split(' ')[1];
+        console.log("token is:", token);
 
         const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
     
@@ -17,17 +18,49 @@ export const middlewareAuth = async (req, res, next) => {
                 message: "User unauthorized"
             });
         }
+
+        if (decodedToken.exp < Date.now() / 1000) {
+            const user = await User.findById(decodedToken.userId);
+            if (!user) {
+                return res.status(403).json({
+                    status: "FAILED",
+                    message: "Invalid user"
+                })
+            }
+
+            const newToken = generateNewToken(user);
+            res.setHeader('Authorization', `Beare ${newToken}`);
+        }
         // assign user to the id associated with the token
-        const user = await User.findById(decodedToken.userId);
-        req.user = user;
+        req.user = decodedToken;
 
         next();
 
     } catch (error) {
-        console.log(error)
-        return res.status(401).json({
+        if (error instanceof jwt.TokenExpiredError || error instanceof jwt.JsonWebTokenError) {
+            console.log(error);
+            return res.status(401).json({
+                status: "FAILED",
+                message: `Token expired`
+            });
+        }
+        res.status(500).json({
             status: "FAILED",
-            message: `Authentication failed ${error}`
-        });
-    }
+            message: "Internal server error"
+        })
+
+    }      
+}
+
+const generateNewToken = (user) => {
+
+    const token = jwt.sign({
+        userId: user._id,
+        email: user.email,
+    },
+    process.env.SECRET_KEY,
+    {expiresIn: '24h'}
+    );
+
+    return token;
 }
