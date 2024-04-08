8a96eae751b1ba9e604cfbd65d17560b826ea148
betiniakarandut <betiniakarandut@gmail.com>
2024-04-08 22:53:40 +0100

    feat: delete 403 debugged

--- controllers/channelController.js
+++ controllers/channelController.js
@@ -4,7 +4,10 @@ import Video from "../models/videoModel.js";
 export const channel = async (req, res) => {
     try {
         const userId = req.user._id;
+        const username = req.user.username;
+        // console.log('userName:', username);
         const { name, description } = req.body;
+        const channelId = Channel.channelId;
 
         // check if user is authenticated
         if (!userId) {
@@ -15,7 +18,7 @@ export const channel = async (req, res) => {
         }
 
         //check if there is an existing channel with user Id
-        const existingChannel = await Channel.findById({_id: userId});
+        const existingChannel = await Channel.findOne(userId);
         if (existingChannel) {
             return res.status(400).json({
                 status: "FAILED",
@@ -24,40 +27,26 @@ export const channel = async (req, res) => {
         }
         // if user is authenticated, create a channel for that user
         const newChannel = new Channel({
-            name: name,
-            description: description,
+            name,
+            description,
+            userId,
             createdAt: Date.now()
         });
         const savedChannel = await newChannel.save();
         if (!savedChannel) {
-            return res.status(401).json({
+            return res.status(400).json({
                 status: "FAILED",
                 message: "Oops! Something went wrong. Refresh and try again"
-            });
-        }
-
-        const userVideoList = await Video.findById({_id: userId});
-
-        if (!userVideoList) {
-            return res.status(401).json({
-                status: "SUCCESS",
-                message: "User has not uploaded any videos yet",
-                videos: [],
             });
         }
         
         return res.status(200).json({
             status: "SUCCESS",
-            message: `Channel was created for ${userId.name} and your channel name is ${name}`,
+            message: `Channel was created for ${username} and your channel name is ${name}`,
             channelId: savedChannel._id,
             subcribers: savedChannel.subcribers,
-            createdAt: savedChannelChannel.createdAt,
-            videos: [{
-                title: userVideoList.title, 
-                path: userVideoList.filePath, 
-                createdAt: userVideoList.createdAt,
-            }] 
-
+            createdAt: savedChannel.createdAt,
+            videos: [],
         });
     } catch (error) {
         console.log(error);
@@ -72,22 +61,27 @@ export const deleteChannel = async (req, res) => {
     try {
         const channelId = req.params.channelId;
         const userId = req.user._id;
+        // console.log('This is userId', userId);
+        // console.log('This is channelId', channelId);
 
         //ensure user is the owner of the channel
-        if (!userId && !channelId) {
+        const channel = await Channel.findOne({_id: channelId});
+        console.log("This is channel object: ", channel)
+        if (!channel) {
             return res.status(403).json({
                 status: "FAILED",
                 message: "User is unauthorized to perform this operation"
             });
         }
 
-        const findChannelById = await Channel.findOneAndDelete({_id: channelId});
+        const findChannelById = await Channel.findOneAndDelete(channelId);
         if (!findChannelById) {
             return res.status(404).json({
                 status:"FAILED",
                 message: "Not Found: user has no such channel"
             });
         }
+        await Channel.findByIdAndDelete(channelId);
 
         return res.status(200).json({
             status: "SUCCESS",
@@ -109,33 +103,32 @@ export const updateChannel = async (req, res) => {
     try {
         const userId = req.user._id;
         const channelId = req.params.channelId;
+        const { name, description } = req.body;
         if (!userId) {
             return res.status(403).json({
                 status: "FAILED",
-                message: "Unauthorized"
+                message: "Unauthorized",
             });
         }
 
-        const retrieveChannel = await Channel.findOneAndUpdate({_id: channelId});
+        const retrieveChannel = await Channel.findOne({_id: channelId});
         if (!retrieveChannel) {
             return res.status(404).json({
                 status: "FAILED",
                 message: "Not found: No existing channel"
             })
         }
-        // prompt user to tell what actin in the channel
-        const askUser = prompt('Do you want to update channel name?  ').toLowerCase;
-        if (askUser === 'yes') {
-            req.body.name = req.body;
-        } else if (askUser === 'no') {
-            req.body.description = request.body;
-        }
+        // check what action wants to perform
+        if(name) retrieveChannel.name = name;
+        if(description) retrieveChannel.description = description;
+
+        await retrieveChannel.save();
 
         return res.status(200).json({
             status: "SUCCESS",
             message: "Your channel was updated successfully",
-            name: req.body.name,
-            description: req.body.description
+            name: retrieveChannel.name,
+            description: retrieveChannel.description
         })
     } catch (error) {
         console.log(error);
