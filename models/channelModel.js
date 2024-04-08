4a961b4e055055b4d83f2604ab4b199a90fec842
betiniakarandut <betiniakarandut@gmail.com>
2024-04-08 22:54:33 +0100

    chore: channel model modified

--- models/channelModel.js
+++ models/channelModel.js
@@ -1,13 +1,21 @@
 import mongoose from "mongoose";
+// import { v4 as uuidv4 } from "uuid"
 
 const channelModelShema = new mongoose.Schema({
     name: String,
     description: String,
     subcribers: Number,
-    creatorId: {type: String, ref: 'User'},
+    channelId: {type: String, unique: true,},
+    creatorId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
     createdAt: Date,
 });
 
+// channelModelShema.pre('save', function generateId(next){
+//     if(!this.channel) {
+//         this.channelId = uuidv4();
+//     }
+//     next();
+// });
 const Channel = mongoose.model("Channel", channelModelShema);
 
 export default Channel;
