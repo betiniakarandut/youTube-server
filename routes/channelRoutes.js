0bfff7ecba710cacb1a4cc0cadb28e109e698fa1
betiniakarandut <betiniakarandut@gmail.com>
2024-04-08 22:55:52 +0100

    chore: channel route modified

--- routes/channelRoutes.js
+++ routes/channelRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { channel } from "../controllers/channelController.js";
+import { 
+    channel, 
+    deleteChannel, 
+    updateChannel
+} from "../controllers/channelController.js";
 import { middlewareAuth } from "../middlewares/userAuth.js";
 
 const channelRoute = express.Router();
 
-channelRoute.get('/:channelId', middlewareAuth, channel);
+channelRoute.post('/', middlewareAuth, channel);
+channelRoute.delete('/:channelId/delete', middlewareAuth, deleteChannel);
+channelRoute.put('/:channelId/update', middlewareAuth, updateChannel);
 
 export default channelRoute;
