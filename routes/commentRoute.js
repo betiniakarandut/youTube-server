813d1ac8e2612912caacce0a4da05770aa29be91
betiniakarandut <betiniakarandut@gmail.com>
2024-04-09 13:10:34 +0100

    feat: likes and dislikes routes

--- routes/commentRoute.js
+++ routes/commentRoute.js
@@ -4,12 +4,16 @@ import {
     comment, 
     updateComment, 
     deleteComment,
+    likeComment,
+    unlikeComment,
 } from "../controllers/commentsController.js";
 
 const commentRoute = express.Router();
 
 commentRoute.post('/:videoId', middlewareAuth, comment);
-commentRoute.put('/:commentId', middlewareAuth, updateComment);
-commentRoute.delete('/:commentId', middlewareAuth, deleteComment);
+commentRoute.put('/:commentId/update', middlewareAuth, updateComment);
+commentRoute.delete('/:commentId/delete', middlewareAuth, deleteComment);
+commentRoute.post('/:commentId/like', middlewareAuth, likeComment);
+commentRoute.delete('/:commentId/dislike', middlewareAuth, unlikeComment);
 
 export default commentRoute;
