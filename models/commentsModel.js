1bcd8e8364bc3e7264706c4fbacfcf48047c1d4a
betiniakarandut <betiniakarandut@gmail.com>
2024-04-23 20:46:59 +0100

    feat: list:sub comment

--- models/commentsModel.js
+++ models/commentsModel.js
@@ -4,6 +4,11 @@ const commentsSchema = new mongoose.Schema({
     userId: {type:mongoose.Schema.Types.ObjectId, ref: 'User'},
     videoId: {type: mongoose.Schema.Types.ObjectId, ref: 'Video'},
     text: String,
+    subComments: [{
+        text: String,
+        userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+        createdAt: {type: Date, default: Date.now()},
+    }],
     likes: {type: Array, default: 0},
     dislikes: {type: Array, default: 0},
     createdAt: Date,
