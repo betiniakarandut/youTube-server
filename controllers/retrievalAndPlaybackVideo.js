066474b8f5e835a3fdcc13cd6ae59105d91a0adb
betiniakarandut <betiniakarandut@gmail.com>
2024-04-30 04:34:25 +0100

    feat: search and retrieve video from database

---
+++ controllers/retrievalAndPlaybackVideo.js
@@ -0,0 +1,66 @@
+import Video from "../models/videoModel.js";
+
+export const retrieveVideo = async (req, res) => {
+    try {
+        const userId = req.body;
+        const videoId = req.params.videoId;
+
+        const videos = await Video.findById({_id: videoId});
+        if (!videoId) {
+            return res.status(404).json({
+                status: "FAILED",
+                message: "Video not found!"
+            });
+        }
+
+        return res.status(200).json({
+            status: "SUCCESS",
+            message: "Retrieved!",
+            videos
+        });
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            status: "FAILED",
+            message: "Internal server error"
+        });
+    }
+}
+
+export const searchVideos = async (req, res) => {
+    try {
+        const { search } = req.query;
+        const video = await Video.find({
+            $or: [
+                {description: {$regex: new RegExp(search, "i")}},
+                {title: {$regex: new RegExp(search, "i")}},
+                {videoId: {$regex: new RegExp(search, "i")}},
+            ]
+        });
+
+        return res.status(200).json({
+            status: "SUCCESS",
+            message: "videos retrieved successfully",
+            video,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(200).json({
+            status: "FAILED",
+            message: "Internal server error"
+        });
+    }
+}
+
+export const playBackVideo = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            status: "FAILED",
+            message: "Internal server error"
+        });
+    }
+}
