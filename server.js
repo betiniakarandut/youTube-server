aad45501463849c53b90dd8f296396dc0f7b9469
betiniakarandut <betiniakarandut@gmail.com>
2024-04-30 23:29:57 +0100

    feat: consume api

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ import likeRoute from "./routes/likeRoute.js";
 import channelRoute from "./routes/channelRoutes.js";
 import viewsRoute from "./routes/viewsRoutes.js";
 import searchRoute from "./routes/searchAndPlayBackRoute.js";
+import paginationRoute from "./routes/videoPaginationRoute.js";
 
 const app = express();
 
@@ -32,7 +33,8 @@ app.use('/comments', commentRoute);
 app.use('/likes', likeRoute);
 app.use('/channel', channelRoute);
 app.use('/views', viewsRoute);
-app.use('/search', searchRoute)
+app.use('/search', searchRoute);
+app.use('/pagination', paginationRoute);
 
 mongoose.connect(dbConnStr, { useNewUrlParser: true })
 .then( () => {
