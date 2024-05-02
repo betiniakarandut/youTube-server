46dc24045436f1869012fa5a2fbdde675daa1f05
betiniakarandut <betiniakarandut@gmail.com>
2024-05-02 05:27:46 +0100

    feat: playback

--- models/videoModel.js
+++ models/videoModel.js
@@ -4,10 +4,8 @@ const videoModelSchema = new mongoose.Schema({
     title: String,
     description: String,
     filePath: String,
+    playback: String,
     creatorId: String,
-    videoId: {
-        type: String,   
-        unique: true},
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     createdAt: { type: Date, default: Date.now() },
     views: { type: Number, default: 0},
