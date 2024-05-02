1d85570510a95061546edb60e5769bd02debf279
betiniakarandut <betiniakarandut@gmail.com>
2024-05-02 05:28:30 +0100

    feat: playback route

--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -2,11 +2,24 @@ import express from "express";
 import multer from "multer";
 import { middlewareAuth } from "../middlewares/userAuth.js";
 import { videoUpload } from "../controllers/videoController.js";
+import { pagination } from "../controllers/videoPagination.js";
+import { playBackVideo } from "../controllers/retrievalAndPlaybackVideo.js";
 
 const videoRoute = express.Router();
 
-const uploads = multer({dest: 'uploads/'});
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+});
 
-videoRoute.post('/upload', middlewareAuth, uploads.single('video'), videoUpload);
+const upload = multer({storage: storage});
+
+videoRoute.post('/upload', upload.fields([{name:'file', maxCount: 1 }]), middlewareAuth, videoUpload);
+videoRoute.get('/pagination', pagination);
+videoRoute.get('/playback/:videoId', playBackVideo)
 
 export default videoRoute;
