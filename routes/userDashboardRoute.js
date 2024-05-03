d032100e48b65ae2dbbec33a1249231cd2cfac9b
betiniakarandut <betiniakarandut@gmail.com>
2024-05-03 20:31:39 +0100

    feat: user dashboard route

---
+++ routes/userDashboardRoute.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { middlewareAuth } from "../middlewares/userAuth.js";
+import multer from "multer";
+import { userDashboard } from "../controllers/userDashboard.js";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+});
+
+const upload = multer({storage: storage});
+
+const dashboardRoute = express.Router();
+
+dashboardRoute.post('/dashboard', upload.single('file'), middlewareAuth, userDashboard);
+
+export default dashboardRoute;
