b40870b471aee1bd61a5afc3df2204fab853f734
betiniakarandut <betiniakarandut@gmail.com>
2024-05-10 23:11:36 +0100

    feat: unsubscribe route

--- routes/channelRoutes.js
+++ routes/channelRoutes.js
@@ -1,15 +1,19 @@
 import express from "express";
 import { 
-    channel, 
+    createChannel, 
     deleteChannel, 
+    subscribeChannel, 
+    unSubscribeChannel, 
     updateChannel
 } from "../controllers/channelController.js";
 import { middlewareAuth } from "../middlewares/userAuth.js";
 
 const channelRoute = express.Router();
 
-channelRoute.post('/', middlewareAuth, channel);
+channelRoute.post('/create', middlewareAuth, createChannel);
 channelRoute.delete('/:channelId/delete', middlewareAuth, deleteChannel);
 channelRoute.put('/:channelId/update', middlewareAuth, updateChannel);
+channelRoute.post('/subscribe', middlewareAuth, subscribeChannel);
+channelRoute.delete('/unsubscribe', middleware, unSubscribeChannel)
 
 export default channelRoute;
