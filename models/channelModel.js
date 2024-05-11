4fac95b437d3f8017ec761b637abfe415db90b81
betiniakarandut <betiniakarandut@gmail.com>
2024-05-11 02:27:53 +0100

    chore: array

--- models/channelModel.js
+++ models/channelModel.js
@@ -4,7 +4,7 @@ const channelModelShema = new mongoose.Schema({
     name: String,
     description: String,
     subcribersCount: {type: Number, default: 0},
-    subscribers: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    subscribers: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
     creatorId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
     createdAt: Date,
 });
