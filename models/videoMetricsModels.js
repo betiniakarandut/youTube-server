40108f73d62526a81ceb610bfa31d7e1b491431c
betiniakarandut <betiniakarandut@gmail.com>
2024-05-15 20:26:45 +0100

    chore: obj

--- models/videoMetricsModels.js
+++ models/videoMetricsModels.js
@@ -5,6 +5,8 @@ const videoMetricsSchema = new mongoose.Schema({
     dislikes: Number,
     comments: Number,
     views: Number,
+    videoId: {type: mongoose.Schema.Types.ObjectId, ref: 'Video'},
+    createdAt: {type: Date, default: Date.now()},
 });
 
 const VideoMetrics = mongoose.model('VideoMetrics', videoMetricsSchema);
