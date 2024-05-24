e6558120e4c07c308291308e1e33248d98fd558a
betiniakarandut <betiniakarandut@gmail.com>
2024-05-24 05:55:59 +0100

    chore: added more annotations

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,6 @@
 import express from 'express';
+const userRoutes = express.Router();
+
 import { 
     signUp,
     signIn,
@@ -10,16 +12,82 @@ import {
 import { facebookSignUp, githubSignUp } from '../controllers/githubFacebookIntegration.js';
 import { middlewareAuth } from '../middlewares/userAuth.js';
 
-
-const userRoutes = express.Router();
-
+/**
+ * @swagger
+ * /user/signup:
+ *   post:
+ *     summary: Create a new user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: User created successfully
+ *       400:
+ *         description: Invalid input
+ */
 userRoutes.post('/signup', signUp);
+
+/**
+ * @swagger
+ * /user/signin:
+ *   post:
+ *     summary: sign in a user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: User signed in successfully
+ *       400:
+ *         description: Invalid input
+ */
 userRoutes.post('/signin', signIn);
 userRoutes.post('/sendotpverificationsms', sendOTPVerificationEmailAndSMS);
+/**
+ * @swagger
+ * /user/verifyotp:
+ *   post:
+ *     summary: verify a user OTP
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/userOTPVerification'
+ *     responses:
+ *       200:
+ *         description: User signed in successfully
+ *       400:
+ *         description: Invalid token or expired token 
+ */
 userRoutes.post('/verifyotp', verifyOTP);
 userRoutes.get('/verify/:userId/:uniqueString', verify);
 userRoutes.post('/githubsignup', githubSignUp);
 userRoutes.post('/facebooksignup', facebookSignUp);
+/**
+ * @swagger
+ * /user/delete:
+ *   post:
+ *     summary: admin can delete a user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: User signed in successfully
+ *       400:
+ *         description: Invalid input
+ */
 userRoutes.delete('/:deletedUserId/deleteuser', middlewareAuth, deleteUser);
 
 export default userRoutes;
