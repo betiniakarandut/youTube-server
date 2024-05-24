119f92aed0781125e9c52e5090702d62fe2f31fe
betiniakarandut <betiniakarandut@gmail.com>
2024-05-24 05:56:52 +0100

    chore: added more schemas

---
+++ swagger.js
@@ -0,0 +1,115 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'YouTube Server API',
+    version: '1.0.0',
+    description: 'API documentation for YouTube Server',
+  },
+  servers: [
+    {
+      url: 'http://localhost:5000',
+      description: 'Development server'
+    },
+  ],
+  components: {
+    schemas: {
+      User: {
+        type: 'object',
+        required: ['username', 'email', 'phone', 'password'],
+        properties: {
+          username: {
+            type: 'string',
+            description: 'Username',
+          },
+          email: {
+            type: 'string',
+            format: 'email',
+            description: 'The user\'s email',
+          },
+          phone: {
+            type: 'integer',
+            description: 'The user\'s mobile number',
+          },
+          password: {
+            type: 'string',
+            description: 'The user\'s password',
+          },
+        },
+      },
+    },
+  },
+  components: {
+    schemas: {
+      userOTPVerification: {
+        type: 'object',
+        required: ['userId', 'otp'],
+        properties: {
+          userId: {
+            type: 'string',
+            description: 'User ID',
+          },
+          otp: {
+            type: 'string',
+            description: 'OTP received by user',
+          },
+        },
+      },
+    },
+  },
+  components: {
+    schemas: {
+      Video: {
+        type: 'object',
+        required: ['title', 'description', 'filepath', 'category'],
+        properties: {
+          title: {
+            type: 'string',
+            description: 'Video title',
+          },
+          description: {
+            type: 'string',
+            description: 'Video description',
+          },
+          filepath: {
+            type: 'string',
+            description: 'Video path',
+          },
+          category: {
+            type: 'string',
+            description: 'Video category',
+          },
+        },
+      },
+    },
+  },
+  components: {
+    schemas: {
+      Comments: {
+        type: 'object',
+        required: ['text'],
+        properties: {
+          text: {
+            type: 'string',
+            description: 'comments',
+          },
+        },
+      },
+    },
+  },
+};
+
+const options = {
+  definition: swaggerDefinition,
+  apis: ['./routes/*.js'],
+}
+
+const swaggerSpec = swaggerJSDoc(options);
+
+const swaggerSetup = (app) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
+
+export default swaggerSetup;
