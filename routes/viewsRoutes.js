efb84d1caddc0f91ee0f0dfc297a984ac1386a09
betiniakarandut <betiniakarandut@gmail.com>
2024-05-28 04:33:33 +0100

    fixes: docs in views controller

--- routes/viewsRoutes.js
+++ routes/viewsRoutes.js
@@ -8,27 +8,23 @@ const viewsRoute = express.Router();
  * @swagger
  * /views/{videoId}:
  *  get:
- *    summary: Get views on videos
+ *    summary: Get the number of views on a particular video
+ *    description: Authenticated user is able vieww
  *    security:
  *      - BearerAuth: []
- *    description: Authenticated user can see number of views on a particular video
  *    parameters:
  *      - in: path
  *        name: videoId
  *        required: true
- *  responses:
- *    200:
- *      description: Views on video
- *      content:
- *        application/json:
- *          schema:
- *            types: object
- *    404:
- *      description: Video not found
- *    400:
- *      description: Invalid token or token expired
- *    500:
- *      description: Internal server error
+ *    responses:
+ *      200:
+ *        description: views successful
+ *      400:
+ *        description: Failed to retrieve views.
+ *      401:
+ *        description: Unauthorized - token is required
+ *      500:
+ *        description: Internal server error
  */
 viewsRoute.get('/:videoId', middlewareAuth, views);
 
