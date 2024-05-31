0ce1d28131930b3e111d706437e984480da3cd59
betiniakarandut <betiniakarandut@gmail.com>
2024-05-31 03:25:54 +0100

    feat: bitpay route docs

--- routes/bitpayRoute.js
+++ routes/bitpayRoute.js
@@ -6,6 +6,53 @@ import {
 
 const bitpayRoute = express.Router();
 
+/**
+ * @swagger
+ * /payments/bitpay/invoice:
+ *  post:
+ *    summary: Creates invoice for a user
+ *    security:
+ *      - BeaererAuth: []
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              amount:
+ *                type: integer
+ *              currency:
+ *                type: string
+ *              orderId:
+ *                type: string
+ *              itemDesc:
+ *                type: string
+ *              buyerEmail:
+ *                type: string
+ *    responses:
+ *      201:
+ *        description: Invoice created!
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                amount:
+ *                  type: integer
+ *                currency:
+ *                  type: string
+ *                orderId:
+ *                  type: string
+ *                itemDesc:
+ *                  type: string
+ *                buyerEmail:
+ *                  type: string
+ *      401:
+ *        description: Unauthorized - token expired
+ *      500:
+ *        description: Failed to create invoice. Error => Token must be required
+ */
 bitpayRoute.post('/bitpay/invoice', createBitPayInvoice);
 bitpayRoute.get('/bitpay/invoice', getBitPayInvoice);
 
