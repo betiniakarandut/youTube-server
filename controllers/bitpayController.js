07cf8f55da35c0c8a74e78132d749e6a02a9d8eb
betiniakarandut <betiniakarandut@gmail.com>
2024-05-31 04:01:37 +0100

    chore: user exists

--- controllers/bitpayController.js
+++ controllers/bitpayController.js
@@ -2,6 +2,13 @@ import PaymentService from '../service/bitpayService.js';
 
 export const createBitPayInvoice = async (req, res) => {
     try {
+        const userId = req.user._id;
+        if(!userId){
+            res.status(401).json({
+                status: "Failed",
+                message: "Unauthorized"
+            });
+        }
         const response = await PaymentService.createInvoice(req.body);
         res.status(201).json({ status: "Success", data: response });
     } catch (error) {
@@ -11,6 +18,13 @@ export const createBitPayInvoice = async (req, res) => {
 
 export const getBitPayInvoice = async (req, res) => {
     try {
+        userId = req.user._id;
+        if(!userId){
+            res.status(401).json({
+                status: "Failed",
+                message: "Unauthorized"
+            });
+        }
         const response = await PaymentService.getInvoice(req.query.invoiceId);
         res.status(200).json({ status: "Success", data: response });
     } catch (error) {
