5cac2e3d4e689d93bd4d625c4474ad17c510c036
betiniakarandut <betiniakarandut@gmail.com>
2024-06-05 09:59:14 +0100

    feat: create profile route

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import { middlewareAuth } from '../middlewares/userAuth.js';
-import { getUserProfile } from '../controllers/profileController.js';
+import { createUserProfile, deleteProfile, getUserProfile, updateProfile } from '../controllers/profileController.js';
 
 const profileRoute = express.Router();
 
 profileRoute.get('/', middlewareAuth, getUserProfile);
+profileRoute.delete('/:profileId', middlewareAuth, deleteProfile);
+profileRoute.put('/update', middlewareAuth, updateProfile);
+profileRoute.post('/create', middlewareAuth, createUserProfile);
 
 export default profileRoute;
