ff4b1550707372b0fc9798e8ed34a50b7d138c5b
betiniakarandut <betiniakarandut@gmail.com>
2024-06-05 09:58:09 +0100

    feat: third party data

---
+++ server-auth/clientData.js
@@ -0,0 +1,9 @@
+export const clients = [
+    {
+      clientId: 'client-id-1',
+      clientSecret: 'client-secret-1',
+      redirectUris: ['http://localhost:80'],
+      grant_type: 'authorization_code'
+    }
+  ];
+  
