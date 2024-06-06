23ff787c271abd0546da6652c2d012bab68778b9
betiniakarandut <betiniakarandut@gmail.com>
2024-06-06 21:12:41 +0100

    feat: country object

--- models/profileModel.js
+++ models/profileModel.js
@@ -1,18 +1,27 @@
 import mongoose from "mongoose";
 
+const countrySchema = new mongoose.Schema({
+    label: {type: String, required: false},
+    code: {type: String, required: false},
+    currency: {type: String, required: false},
+    _id: false,
+})
+
 const profileSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     full_name: { type: String, required: true },
-    email: { type: String, ref: 'User', required: true },
-    phone: { type: String, ref: 'User', required: true },
+    email: { type: String, ref: 'User' },
+    phone: { type: String, ref: 'User' },
     address: { type: String, required: true },
     zip_code: { type: String, required: true },
     hobby: { type: String },
     bio: { type: String, required: true },
+    maiden_name: {type: String, required: true},
     profile_image: { type: String },
     date_of_birth: { type: Date, required: true },
     gender: { type: String, required: true },
     nationality: { type: String, required: true },
+    country: {type: countrySchema, default: {}},
     createdAt: { type: Date, default: Date.now }
 });
 
