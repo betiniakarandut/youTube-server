f7ff552da9f9c1fa87d517c6f7a12153f122b8d7
betiniakarandut <betiniakarandut@gmail.com>
2024-06-06 17:29:50 +0100

    chore: new upload

---
+++ utils/passport.js
@@ -0,0 +1,9 @@
+import session from "express-session";
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+
+const passportAuth = (app) => {
+    app.use()
+}
+
+export default passportAuth;
