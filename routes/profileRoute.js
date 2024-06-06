8e694ae0fdf096b98e1a921596e2fbd2098e5845
betiniakarandut <betiniakarandut@gmail.com>
2024-06-06 17:28:32 +0100

    chore: profileId in route

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -1,12 +1,29 @@
 import express from 'express';
+import multer from 'multer';
 import { middlewareAuth } from '../middlewares/userAuth.js';
-import { createUserProfile, deleteProfile, getUserProfile, updateProfile } from '../controllers/profileController.js';
+import { 
+    createUserProfile, 
+    deleteProfile, 
+    getUserProfile, 
+    updateProfile 
+} from '../controllers/profileController.js';
 
 const profileRoute = express.Router();
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'upload')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+});
+
+const upload = multer({storage: storage});
+
 profileRoute.get('/', middlewareAuth, getUserProfile);
 profileRoute.delete('/:profileId', middlewareAuth, deleteProfile);
-profileRoute.put('/update', middlewareAuth, updateProfile);
-profileRoute.post('/create', middlewareAuth, createUserProfile);
+profileRoute.put('/:profileId/update', middlewareAuth, updateProfile);
+profileRoute.post('/create', upload.fields([{ name: 'file', maxCount: 1 }]), middlewareAuth, createUserProfile);
 
 export default profileRoute;
