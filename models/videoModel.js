f0194f70885f4912c1c736491de5a06d87957922
betiniakarandut <betiniakarandut@gmail.com>
2024-06-09 01:29:41 +0100

    chore: viewsId

--- models/videoModel.js
+++ models/videoModel.js
@@ -11,6 +11,7 @@ const videoModelSchema = new mongoose.Schema({
     commentCount: {type: Number, default: 0},
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     createdAt: { type: Date, default: Date.now() },
+    viewsId: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
     views: { type: Number, default: 0},
     dislikes: {type: Number, default: 0},
     likes: [{type: mongoose.Schema.Types.ObjectId, ref: 'Likes'}],
