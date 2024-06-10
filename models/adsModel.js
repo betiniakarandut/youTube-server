8d5909d7d3061b788aab3983f9fa6e60729ae491
betiniakarandut <betiniakarandut@gmail.com>
2024-06-10 12:51:45 +0100

    chore: add_duration

--- models/adsModel.js
+++ models/adsModel.js
@@ -2,9 +2,11 @@ import mongoose from "mongoose";
 
 const adSchema = new mongoose.Schema({
     url: { type: String},
+    playback_url: {type: String},
     title: {type: String},
     description: {type: String},
-    duration: { type: Number},
+    ad_duration: { type: Number},
+    createdAt: {type: Date, default: Date.now}
   });
   
   const Ad = mongoose.model('Ad', adSchema);
